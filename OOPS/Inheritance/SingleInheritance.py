7f38d85d406232a39aba8d9ff1d5070a2f96c4e3
siva <gspr991@gmail.com>
2024-07-15 21:06:42 +0530

    updated the Single Inheritance Python code with doc_strings

--- OOPS/Inheritance/SingleInheritance.py
+++ OOPS/Inheritance/SingleInheritance.py
@@ -1,7 +1,46 @@
 
 class Person:
+    
+    """
+    A class used to represent a Person.
+    
+    Attributes
+    ----------
+    name : str
+        Name of the Person.
+    age : int
+        Age of the Person
+    gender : str
+        Gender of the Person
+    email : str
+        Email of the Person
+    phoneNumber : int
+        Phone Number of the Person
+    address : str
+        Address of the Person
+    Methods
+    -------
+    getDetails():
+        Returns a string containing all the details name, age, gender, email, phoneNumber, address of a Person.
+    """
 
-    def __init__(self, name, age, gender, email, phoneNumber, address) -> None:
+    def __init__(self, name: str, age: int, gender: str, email: str, phoneNumber: int, address: str) -> None:
+        """
+        Parameters
+        ----------
+        name : str
+            Name of the Person.
+        age : int
+            Age of the Person
+        gender : str
+            Gender of the Person
+        email : str
+            Email of the Person
+        phoneNumber : int
+            Phone Number of the Person
+        address : str
+            Address of the Person
+        """
         self.name = name
         self.age = age
         self.gender = gender
@@ -9,17 +48,72 @@ class Person:
         self.phoneNumber = phoneNumber
         self.address = address
 
-    def getDetails(self):
+    def getDetails(self) -> str:
+        """
+        Returns
+        -------
+        str
+            Returns a string containing all the details name, age, gender, email, phoneNumber, address of a Person.
+        """
         return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}, Email: {self.email}, Phone Number: {self.phoneNumber}, Address: {self.address}"
     
 
 class Employee(Person):
+    """
+    A class used to represent a Employee.
+    
+    Attributes
+    ----------
+    name : str
+        Name of the Employee.
+    empId : int
+        Employee Id of the Employee.
+    age : int
+        Age of the Employee
+    gender : str
+        Gender of the Employee
+    email : str
+        Email of the Employee
+    phoneNumber : int
+        Phone Number of the Employee
+    address : str
+        Address of the Employee
+    Methods
+    -------
+    getDetails():
+        Returns a string containing all the details name, age, gender, email, phoneNumber, address of an Employee.
+    """
 
-    def __init__(self, name, empId, age, gender, email, phoneNumber, address):
+    def __init__(self, name: int, empId: int, age: int, gender: str, email: str, phoneNumber: int, address: str):
+        """
+        Parameters
+        ----------
+        name : str
+            Name of the Employee.
+        empId : int
+            Employee Id of the Employee.
+        age : int
+            Age of the Employee
+        gender : str
+            Gender of the Employee
+        email : str
+            Email of the Employee
+        phoneNumber : int
+            Phone Number of the Employee
+        address : str
+            Address of the Employee
+        """
+
         super().__init__(name, age, gender, email, phoneNumber, address)
         self.empId = empId
 
     def getDetails(self):
+        """
+        Returns
+        -------
+        str
+            Returns a string containing all the details name, empId, age, gender, email, phoneNumber, address of an Employee.
+        """
         return f"EmpId: {self.empId}, {super().getDetails()}"
 
 if __name__ == '__main__':
