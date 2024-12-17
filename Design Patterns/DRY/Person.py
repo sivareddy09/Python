fc486424025230f68e0addb15d61005408f6e2ee
sivareddy09 <gspr991@gmail.com>
2024-12-17 12:58:33 +0530

    tested Person.py

--- Design Patterns/DRY/Person.py
+++ Design Patterns/DRY/Person.py
@@ -8,4 +8,8 @@ class Person:
         self.age = age
 
     def display(self):
-        return f"FirstName: {self.firstName}, LastName: {self.lastName},Gender: {self.gender}, Age: {self.age}"
+        return f"FirstName: {self.firstName}, LastName: {self.lastName}, Gender: {self.gender}, Age: {self.age}"
+
+if __name__ == "__main__":
+    mahesh = Person("Mahesh", "Mahesh", "Male", 26)
+    print(mahesh.display())
